21185aecc296233b21c165dfea89b2f8324acb89
edwinnab <edwinnabikeri@gmail.com>
2022-09-26 17:31:15 +0300

    react shooping app

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,11 +1,32 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+function ItemForm({onAddItem}) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    //create a new item, so as to save data to our server
+    const itemData = {
+      name: name,
+      category: category,
+      isInCart: false,
+    };
+    fetch("http://localhost:4000/items", {
+      method: "POST",
+      //specifys we are sending a JSON string in the request
+      headers: {
+        "Content-Type" : "application/json",
+      },
+      //stringified object we are sending
+      body: JSON.stringify(itemData),
+    })
+    .then((r) => r.json())
+    .then((newItem) => onAddItem(newItem));
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
